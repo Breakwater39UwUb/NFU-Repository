ed709c71f3bfed7eac382bef36ccc1c067cde1cd
Breakwater39 <leoandhankmail@gmail.com>
2024-02-26 14:14:37 +0800

    add test.py, test.ipynb

---
+++ ReviewAPP/Packages/db_update.py
@@ -0,0 +1,41 @@
+import pymysql
+from Packages.parse_ngrok import get_connection_args
+
+def db_update(rating: int, text: str, db_name: str):
+    """Update database with new review
+    Args:
+        rating: int in range 1 to 5
+        text: string
+        db: database to connect, default is None
+            which connect to reviews database
+    No returns
+    """
+
+    if db_name is None:
+        db_name = 'reviews'
+
+    try:
+        forward_ip, forward_port = get_connection_args()
+        db = pymysql.connect(host=forward_ip,
+                            port=forward_port,
+                            user="root",
+                            database=db_name,
+                            password="239mikuNFU@~@")
+    except pymysql.err.OperationalError as err:
+        print("Error connecting to MySQL database, check your ngrok host and port.")
+    else:
+        cursor = db.cursor()
+
+        # Update table
+        # this if-else statement is used to debug, delete before publishing
+        if db_name == 'test_con':
+            command = "INSERT INTO test_table (rating, text) VALUES (%s, %s)"
+        else:
+            command = "INSERT INTO reviews (rating, txt) VALUES (%s, %s)"
+
+        review = (rating, text)
+
+        cursor.execute(command, review)
+        db.commit()
+
+        db.close()
---
+++ ReviewAPP/Packages/parse_ngrok.py
@@ -0,0 +1,17 @@
+import os
+def get_connection_args(filepath: str = None):
+    """Returns the connection hostname and port
+    filepath: None -> './ReviewAPP/ngrok.txt'
+    """
+
+    if filepath is None:
+        filename = './ReviewAPP/ngrok.txt'
+    else:
+        filename = './ngrok.txt'
+
+    with open(filename, 'r') as file:
+        hostname = file.readline()
+        forward_ip, forward_port = hostname.split('://')[1].split(':')
+        forward_port = int(forward_port)
+
+        return forward_ip, forward_port
