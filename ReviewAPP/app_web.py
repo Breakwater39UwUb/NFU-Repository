15ca2f6a7c1f6c1203aba621b3486df498873ec9
1234AWEOOOOOO <157381961+1234AWEOOOOOO@users.noreply.github.com>
2024-03-05 01:43:33 +0800

    Update app_web.py

--- ReviewAPP/app_web.py
+++ ReviewAPP/app_web.py
@@ -1,24 +1,27 @@
-from flask import Flask, request, render_template
-from markupsafe import escape
-app = Flask(__name__, template_folder='web')
-@app.route('/', methods=['POST','GET'])
-def Comment():
-    if request.method == "POST":
-       comment = request.form.get("Comment")
-       return f'{comment}'
-    return render_template('main.html')
+from flask import Flask, request, render_template,json
+app = Flask(__name__,template_folder="template") 
+
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def Home():
+   return render_template('main.html')
     
-def FoodpandaLink():
-    if request.method == "POST":
-       foodpandalink = request.form.get("FoodpandaLink")
-       return f'{foodpandalink}'
-    return render_template('main.html')
-
-def GoogleLink():
-    if request.method == "POST":
-       googlelink = request.form.get("GoogleLink")
-       return f'{googlelink}'
-    return render_template('main.html')
-
+@app.route("/comment", methods=['POST'])
+def comment():
+        comment = request.form['Comment']
+        if comment:
+           print(request.form['Comment'])
+           return comment    
+        
+        link = request.form['Link']
+        if link:
+           print(request.form['Link'])
+           return link 
+        
+        
+        
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    app.run(port=8900)
