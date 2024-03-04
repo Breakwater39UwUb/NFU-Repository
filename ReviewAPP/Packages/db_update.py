c6c94dbfd327f9d2d8948f3d4af079f106eb592d
Breakwater39 <leoandhankmail@gmail.com>
2024-03-04 22:42:59 +0800

    add json file upload

--- ReviewAPP/Packages/db_update.py
+++ ReviewAPP/Packages/db_update.py
@@ -1,5 +1,6 @@
 import pymysql
 import csv
+import json
 from Packages.parse_ngrok import get_connection_args
 
 def db_update(rating: int, text: str, db_name: str, ngrok_file: str | None = None):
@@ -19,9 +20,9 @@ def db_update(rating: int, text: str, db_name: str, ngrok_file: str | None = Non
         forward_ip, forward_port = get_connection_args(ngrok_file)
         db = pymysql.connect(host=forward_ip,
                             port=forward_port,
-                            user="root",
+                            user="web",    # root
                             database=db_name,
-                            password="239mikuNFU@~@",
+                            password="password",   # 239mikuNFU@~@
                             charset='utf8mb4')
     except pymysql.err.OperationalError as err:
         print(f'Error connecting to MySQL database, check your ngrok host and port.\nhost: {forward_ip}\nport: {forward_port}')
@@ -42,29 +43,33 @@ def db_update(rating: int, text: str, db_name: str, ngrok_file: str | None = Non
 
         db.close()
 
-def db_upload_file(filename: str, db_name: str, ngrok_file: str | None = None):
+def db_upload_file(filename: str, db_name: str, ngrok_file: str | None = None, file_format: str = None):
     """Upload reviews in given file
     Args:
-        filename: path under ./SaveData/
-        db_name: database to connect, default connect to reviews database
-        ngrok_file: default = './ReviewAPP/ngrok.txt'
+        filename: File under ./SaveData/
+        db_name: Database to connect, default connect to reviews database
+        ngrok_file: Default = './ReviewAPP/ngrok.txt'
+        file_format: Default = 'csv'
     No returns
     """
 
     if db_name is None:
         db_name = 'reviews'
 
+    if file_format is None:
+        file_format = 'csv'
+    elif file_format not in ['csv', 'json']:
+        raise Exception('file_format must be csv or json')
+
     table_name = '`' + filename + '`'
-    # print('---------------------------------------------------->', filename, '--------------------------------')
-    # print('---------------------------------------------------->', table_name, '--------------------------------')
 
     try:
         forward_ip, forward_port = get_connection_args(ngrok_file)
         db = pymysql.connect(host=forward_ip,
                             port=forward_port,
-                            user="root",
+                            user="web",    # root
                             database=db_name,
-                            password="239mikuNFU@~@",
+                            password="password",   # 239mikuNFU@~@
                             charset='utf8mb4')
     except pymysql.err.OperationalError as err:
         print(f'Error connecting to MySQL database, check your ngrok host and port.\nhost: {forward_ip}\nport: {forward_port}')
@@ -77,24 +82,44 @@ def db_upload_file(filename: str, db_name: str, ngrok_file: str | None = None):
         print('------------------>', command)
         cursor.execute(command)
 
-        filename = './SaveData/' + filename + '.csv'
+        filename = './SaveData/' + filename + '.' + file_format
 
-        with open(filename, 'r', encoding='utf-8') as file:
-            lines = csv.reader(file)
-            for line in lines:
-                # Update table
-                # this if-else statement is used to debug, delete before publishing
-                if db_name == 'test_con':
-                    command = f"INSERT INTO {table_name} (rating, txt) VALUES (%s, %s)"
-                else:
-                    command = "INSERT INTO reviews (rating, txt) VALUES (%s, %s)"
-                rating, text  = line[0], line[1]
-                if text == '':
-                    continue
-                # rating = int(rating)
-                review = (rating, text)
-                # print(review)
-                cursor.execute(command, review)
-                db.commit()
+        if file_format == 'csv':
+            with open(filename, 'r', encoding='utf-8') as csv_file:
+                lines = csv.reader(csv_file)
+                for line in lines:
+                    # Update table
+                    # this if-else statement is used to debug, delete before publishing
+                    if db_name == 'test_con':
+                        command = f"INSERT INTO {table_name} (rating, txt) VALUES (%s, %s)"
+                    else:
+                        command = "INSERT INTO reviews (rating, txt) VALUES (%s, %s)"
+                    rating, text  = line[0], line[1]
+                    if text == '':
+                        continue
+                    # rating = int(rating)
+                    review = (rating, text)
+                    # print(review)
+                    cursor.execute(command, review)
+                    db.commit()
+        elif file_format == 'json':
+            with open(filename, 'r', encoding='utf-8') as json_file:
+                lines = json.load(json_file)
+                for line in lines:
+                    # Update table
+                    # this if-else statement is used to debug, delete before publishing
+                    if db_name == 'test_con':
+                        command = f"INSERT INTO {table_name} (rating, txt) VALUES (%s, %s)"
+                    else:
+                        command = "INSERT INTO reviews (rating, txt) VALUES (%s, %s)"
+                    rating, text  = line['rating'], line['comment']
+                    if text == '':
+                        continue
+                    # rating = int(rating)
+                    review = (rating, text)
+                    # print(review)
+                    cursor.execute(command, review)
+                    db.commit()
+                
         db.close()
-        print('File uploaded successfully')
+        print(f'{filename} uploaded successfully.')
