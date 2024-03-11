f40a2e47e05d5e2391c4c85e941ea0661eddf9ce
Breakwater39 <leoandhankmail@gmail.com>
2024-03-11 13:23:36 +0800

    add route for /get_reviews

--- ReviewAPP/app.py
+++ ReviewAPP/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, request, render_template,json
+from flask import Flask, request, render_template, json
 import os
 from Packages.scraper import get_reviews
-app = Flask(__name__,template_folder="web/templates/") 
+flask_template_path = 'web/'	# web/templates/
+home_page = 'web.html'	# main.html
+predict_page = 'predict.html'
 
-
-
+app = Flask(__name__,
+			template_folder=flask_template_path,
+			static_folder=flask_template_path,
+			static_url_path='')
 app.debug = True
 
 @app.route("/")
 def Home():
-   return render_template('main.html')
+	return render_template(home_page)
 
 @app.route("/comment", methods=['POST'])
 def comment():
@@ -26,9 +30,14 @@ def comment():
 		except Exception:
 			print(os.getcwd())
 		finally:
-			return link 
-        
-        
+			return link
+		
+@app.route("/get_reviews", methods=['POST'])
+def get_reviews():
+	
+	print(request.form['star'])
+	print(request.form['txt'])
+	return render_template(predict_page)
         
 if __name__ == "__main__":
-    app.run(port=8900)
+	app.run(port=8900)
