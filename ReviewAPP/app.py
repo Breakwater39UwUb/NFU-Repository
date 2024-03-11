6f6d8ec55d948a9f3abf08255008724533940b7d
1234AWEOOOOOO <yee65569@gmail.com>
2024-03-11 18:51:13 +0800

    get args from web

--- ReviewAPP/app.py
+++ ReviewAPP/app.py
@@ -4,6 +4,7 @@ from Packages.scraper import get_reviews
 flask_template_path = 'web/'	# web/templates/
 home_page = 'web.html'	# main.html
 predict_page = 'predict.html'
+platform = ''
 user_rating = 1
 bert_rating = 1
 
@@ -22,7 +23,8 @@ def comment():
 	comment = request.form['Comment']
 	if comment:
 		print(request.form['Comment'])
-		return comment    
+		return comment  
+	
 
 	link = request.form['Link']
 	if link:
@@ -34,27 +36,35 @@ def comment():
 		finally:
 			return link
 		
-@app.route("/get_reviews", methods=['POST'])
-def get_reviews():
+@app.route("/get_user_review", methods=['POST'])
+def get_user_review():
 	
 	print(request.form['star'])
 	print(request.form['txt'])
-	return render_template(predict_page)
+	data = request.form['star']
+	return render_template(predict_page, DATA = data)
 
-@app.route("/get_url", methods=['GET'])
+@app.route("/get_url", methods=['GET', 'POST'])
 def get_url():
-	if request.args.get('platform') is None:
-		raise ValueError('Please select a platform.')
-	
-	if request.args.get('url') is None :
+	scrape_url = request.get_json()
+	if scrape_url is None :
 		raise ValueError('Please input a url under "Overview tab".')
 	
-	if request.args.get('platform') == 'Googlemaps':
+	if platform == 'Googlemaps':
+		print(scrape_url)
+		get_reviews(url=scrape_url)
+
+	if platform == 'Foodpanda':
 		pass
 
-	if request.args.get('platform') == 'Foodpanda':
-		pass
+	return render_template(home_page)
 
+@app.route("/get_platform", methods=['GET','POST'])
+def get_platform():
+	output = request.get_json()
+	global platform
+	platform = output
+	print(platform)
 	return render_template(home_page)
         
 if __name__ == "__main__":
