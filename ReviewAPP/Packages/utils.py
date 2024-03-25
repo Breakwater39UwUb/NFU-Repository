0a628f802dae0c0837eeb89109409312cc96bee7
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-03-25 14:40:59 +0800

    some functions for app

--- ReviewAPP/Packages/utils.py
+++ ReviewAPP/Packages/utils.py
@@ -1,16 +1,17 @@
 import glob
+import os
 
-def check_loacal_cache(query: str, query_dir: str = 'SaveData\\', file_type: str = '.json'):
+def check_loacal_cache(query: str, query_dir: str = 'SaveData', file_type: str = '.json'):
     '''Check if the given file is in local directory or database
     
     query: target file
     query_dir: target directory
     file_type: target file extension
     '''
-
-    files = glob.glob(query_dir+'*.'+file_type)
+    search = os.path.join(query_dir,'*')+'.'+file_type
+    files = glob.glob(search)
     for file in files:
         if query in file:
             return True
-        
+
     return False
