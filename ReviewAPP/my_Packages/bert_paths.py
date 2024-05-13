1eedea5810b2c80507c818267200cb49484fbf10
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-05-14 00:28:38 +0800

    update to trinary model

--- ReviewAPP/my_Packages/bert_paths.py
+++ ReviewAPP/my_Packages/bert_paths.py
@@ -1,7 +1,7 @@
 pkl_file = 'trained_model/data_features.pkl'
 vocab_file = 'trained_model/bert-base-chinese-vocab.txt'
 multi_label_model = 'trained_model/model'
-model_file = 'trained_model/pytorch_model.bin'
+model_file = 'trained_model/model.safetensors'
 config_file = 'trained_model/config.json'
 ModelName = "trained_model"
-ClassNum = 5
+ClassNum = 3
