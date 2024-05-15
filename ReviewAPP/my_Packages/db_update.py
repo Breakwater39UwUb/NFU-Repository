2790f8f5585a04cd65c901513c5486f271fc4f66
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-05-15 12:03:43 +0800

    update

--- ReviewAPP/my_Packages/db_update.py
+++ ReviewAPP/my_Packages/db_update.py
@@ -1,20 +1,26 @@
 import pymysql
+from pymysql.constants.ER import DUP_ENTRY
 import csv
 import json
 from my_Packages.parse_ngrok import get_connection_args
+from my_Packages import utils
 
-def db_update(rating: int, text: str, db_name: str, ngrok_file: str | None = None):
+def db_update(time: str,
+              rating: int,
+              text: str,
+              db_name: str = 'reviews',
+              table_name: str = '',
+              ngrok_file: str = None):
     """Update database with new review
     Args:
-        rating: int in range 1 to 5
-        text: string
+        time: review time, ex: 2014/01
+        rating: star in range 1 to 5
+        text: review content
         db_name: database to connect, default connect to reviews database
+        table_name: table to insert
         ngrok_file: default = './ReviewAPP/ngrok.txt'
     No returns
     """
-
-    if db_name is None:
-        db_name = 'reviews'
 
     try:
         forward_ip, forward_port = get_connection_args(ngrok_file)
@@ -26,42 +32,38 @@ def db_update(rating: int, text: str, db_name: str, ngrok_file: str | None = Non
                             charset='utf8mb4')
     except pymysql.err.OperationalError as err:
         print(f'Error connecting to MySQL database, check your ngrok host and port.\nhost: {forward_ip}\nport: {forward_port}')
-    else:
+    try:
         cursor = db.cursor()
 
         # Update table
-        # this if-else statement is used to debug, delete before publishing
-        if db_name == 'test_con':
-            command = "INSERT INTO test_table (rating, text) VALUES (%s, %s)"
-        else:
-            command = "INSERT INTO reviews (rating, txt) VALUES (%s, %s)"
-
-        review = (rating, text)
-
+        command = f"INSERT INTO {table_name}\
+                        (time_range, rating, content) VALUES \
+                        (%s, %s, %s)"
+        review = (time, rating, text)
         cursor.execute(command, review)
         db.commit()
-
+    except pymysql.IntegrityError as e:
+        if e.args[0] == DUP_ENTRY:
+            print(e)
+            pass
+    finally:
         db.close()
 
-def db_upload_file(filename: str, db_name: str, ngrok_file: str | None = None, file_format: str = None):
+def db_upload_file(filename: str,
+                   db_name: str = 'reviews',
+                   ngrok_file: str = './ngrok.txt',
+                   file_format: str = 'json'):
     """Upload reviews in given file
     Args:
         filename: File under ./SaveData/
         db_name: Database to connect, default connect to reviews database
         ngrok_file: Default = './ReviewAPP/ngrok.txt'
-        file_format: Default = 'csv'
+        file_format: Default = 'json'
     No returns
     """
 
-    if db_name is None:
-        db_name = 'reviews'
-
-    if file_format is None:
-        file_format = 'csv'
-    elif file_format not in ['csv', 'json']:
+    if file_format not in ['csv', 'json']:
         raise Exception('file_format must be csv or json')
-
-    table_name = '`' + filename + '`'
 
     try:
         forward_ip, forward_port = get_connection_args(ngrok_file)
@@ -73,53 +75,49 @@ def db_upload_file(filename: str, db_name: str, ngrok_file: str | None = None, f
                             charset='utf8mb4')
     except pymysql.err.OperationalError as err:
         print(f'Error connecting to MySQL database, check your ngrok host and port.\nhost: {forward_ip}\nport: {forward_port}')
-    else:
-        cursor = db.cursor()
-        cursor.execute("SET NAMES utf8mb4")
-        cursor.execute("SET CHARACTER SET utf8mb4")
-        cursor.execute("SET character_set_connection=utf8mb4")
-        command = f'CREATE TABLE IF NOT EXISTS {table_name} (rating INTEGER, txt VARCHAR(1024) COLLATE utf8mb4_unicode_ci)'
-        print('------------------>', command)
-        cursor.execute(command)
 
-        filename = './SaveData/' + filename + '.' + file_format
+    cursor = db.cursor()
+    cursor.execute("SET NAMES utf8mb4")
+    cursor.execute("SET CHARACTER SET utf8mb4")
+    cursor.execute("SET character_set_connection=utf8mb4")
+    tablename = utils.convert_to_tablename(filename)
+    command = f"CREATE TABLE IF NOT EXISTS {tablename}\
+        (time_range CHAR(8),\
+        rating INTEGER,\
+        content VARCHAR(530) PRIMARY KEY\
+        COLLATE utf8mb4_unicode_ci)"
+    # print('------------------>', command)
+    cursor.execute(command)
 
+    with open(filename, 'r', encoding='utf-8') as file:
         if file_format == 'csv':
-            with open(filename, 'r', encoding='utf-8') as csv_file:
-                lines = csv.reader(csv_file)
-                for line in lines:
-                    # Update table
-                    # this if-else statement is used to debug, delete before publishing
-                    if db_name == 'test_con':
-                        command = f"INSERT INTO {table_name} (rating, txt) VALUES (%s, %s)"
-                    else:
-                        command = "INSERT INTO reviews (rating, txt) VALUES (%s, %s)"
-                    rating, text  = line[0], line[1]
-                    if text == '':
-                        continue
-                    # rating = int(rating)
-                    review = (rating, text)
-                    # print(review)
-                    cursor.execute(command, review)
-                    db.commit()
-        elif file_format == 'json':
-            with open(filename, 'r', encoding='utf-8') as json_file:
-                lines = json.load(json_file)
-                for line in lines:
-                    # Update table
-                    # this if-else statement is used to debug, delete before publishing
-                    if db_name == 'test_con':
-                        command = f"INSERT INTO {table_name} (rating, txt) VALUES (%s, %s)"
-                    else:
-                        command = "INSERT INTO reviews (rating, txt) VALUES (%s, %s)"
-                    rating, text  = line['rating'], line['comment']
-                    if text == '':
-                        continue
-                    # rating = int(rating)
-                    review = (rating, text)
-                    # print(review)
-                    cursor.execute(command, review)
-                    db.commit()
-                
+            lines = csv.reader(file)
+        if file_format == 'json':
+            lines = json.load(file)
+
+        for line in lines:
+            # Update table
+            command = f"INSERT INTO {tablename}\
+                (time_range, rating, content) VALUES \
+                (%s, %s, %s)"
+            
+            if file_format == 'csv':
+                time, rating, text  = line[0], line[1], line[2]
+            if file_format == 'json':
+                time, rating, text  = line['time'], line['rating'], line['comment']
+
+            if text == '':
+                continue
+            # rating = int(rating)
+            review = (time, rating, text)
+            # print(review)
+            
+            try:
+                cursor.execute(command, review)
+                db.commit()
+            except pymysql.IntegrityError as e:
+                if e.args[0] == DUP_ENTRY:
+                    print(e)
+                    pass
         db.close()
         print(f'{filename} uploaded successfully.')
