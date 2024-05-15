c4a4354cd2617064e84a88ab85be561d6c7bc7ab
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-05-15 12:03:33 +0800

    add convert_to_tablename()

--- ReviewAPP/my_Packages/utils.py
+++ ReviewAPP/my_Packages/utils.py
@@ -1,5 +1,4 @@
-import glob
-import os
+import glob, os, re
 
 def check_loacal_cache(query: str, query_dir: str = 'SaveData', file_type: str = '.json'):
     '''Check if the given file is in local directory or database
@@ -15,3 +14,18 @@ def check_loacal_cache(query: str, query_dir: str = 'SaveData', file_type: str =
             return True
 
     return False
+
+def convert_to_tablename(filepath: str):
+    '''Convert file path to tablename
+    filepath: like './SaveData/review-Google地圖.json'
+
+    return 
+    ---
+
+    table_name: str
+        table name surrounded by sql escape
+        >>> `table_name`
+    '''
+
+    table_name = '`' + filepath.split('/')[1].split('-')[0] + '`'
+    return table_name
