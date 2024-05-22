3c6317af43ec16660198bb3436f62817d155a77c
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-05-22 20:19:05 +0800

    add chart maker

--- ReviewAPP/app.py
+++ ReviewAPP/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, render_template, json, url_for
 import os
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+from numpy import arange
+import numpy as np
+from datetime import datetime, timedelta
+from collections import defaultdict
 from my_Packages.scraper import get_reviews
 from my_Packages.predictor_1 import review_predict
 from my_Packages.predictor_2 import review_analyze
@@ -159,6 +166,120 @@ def get_chart_args():
     choose one label type, food, price...
 
     '''
+
+def sort_by_month(data: list):
+    '''Count reviews per month
+
+    data: list[tuple]
+        reviews from review_analyze
+    chart_type: str
+        >>> 'plot' or 'bar'
+    '''
+    counts = defaultdict(int)
+
+    # Count the number of data points for each month
+    for d in data:
+        # Extract time
+        time = datetime.strptime(d[2], "%Y/%m")
+        # Increment count
+        counts[time] += 1
+
+    # Sort times and counts for plotting
+    times, counts = zip(*sorted(counts.items()))
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(times, counts)
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m'))
+
+    plt.plot(times, counts)
+    plt.xlabel('Month')
+    plt.ylabel('Data Count')
+    plt.title('Data Count Over Time')
+    plt.show()
+
+def sort_by_label(data: list, label: int):
+    '''Count 1 label per month
+    
+    data: list of data
+        [[label], content, time]
+    label: int of label
+    '''
+    counts = defaultdict(int)
+    label_counts = defaultdict(int)
+
+    # Count the number of data points for each month
+    for d in data:
+        time = datetime.strptime(d[2], "%Y/%m")
+        counts[time] += 1
+
+        if (d[0][label]):
+            label_counts[time] += 1
+        else:
+            label_counts[time] += 0
+
+    # Sort times and counts for plotting
+    times, counts = zip(*sorted(counts.items()))
+    times, label_counts = zip(*sorted(label_counts.items()))
+    fig, ax = plt.subplots(figsize=(12, 6))
+    # ax.plot(times, counts)
+    # ax.xaxis.set_major_locator(mdates.MonthLocator())
+    # ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m'))
+
+    global labels
+    plt.bar(times, counts, 10, color='red', label='Total')
+    plt.plot(times, label_counts, label=f'Label: {labels[label]}')
+    plt.xlabel('Month')
+    plt.ylabel('Data Count')
+    plt.legend()
+    plt.title('label Count per Time')
+    plt.show()
+
+def compare_labels(data: list, comp_labels: list):
+    '''Compare data with given labels
+    
+    data: list[tuple]
+        list of reviews
+    comp_labels: list[int]
+        list of labels to compare
+
+        ex:
+        >>> compare data with 'Food', 'Service', 'Enviroment'
+        >>> [0, 2, 3]
+    '''
+
+    bar_width = 10
+
+    # create list of labels counter
+    label_counts = []
+    for n in range(4):
+        label_counts.append(defaultdict(int))
+
+    for d in data:
+        time = datetime.strptime(d[2], "%Y/%m")
+        for label in comp_labels:
+            if (d[0][label]):
+                label_counts[label][time] += 1
+                continue
+            label_counts[label][time] += 0
+            
+    bar_width = 0.15  # Adjust as needed
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    # Convert times to numbers for plotting
+    all_times = sorted(list(set([time for index in comp_labels for time in label_counts[index].keys()])))
+    time_indices = np.arange(len(all_times))
+
+    for i, index in enumerate(comp_labels):
+        counts = [label_counts[index].get(time, 0) for time in all_times]
+        plt.bar(time_indices + i*bar_width, counts, bar_width, label=labels[index])
+
+    plt.xlabel('Month')
+    plt.ylabel('Data Count')
+    plt.legend()
+    plt.title('Label Count per Time')
+    plt.xticks(time_indices + bar_width, [time.strftime("%Y/%m") for time in all_times])
+    plt.show()
 
 if __name__ == "__main__":
     app.run(port=8900)
